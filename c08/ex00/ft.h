d13ca024ff5c77e996700e6a443ed3d248487dda
Sumyeong-Jang <50583200+Sumyeong-Jang@users.noreply.github.com>
2021-10-28 06:27:31 +0900

    Create ft.h

---
+++ c08/ex00/ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sumjang <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/28 05:19:29 by sumjang           #+#    #+#             */
+/*   Updated: 2021/10/28 05:25:52 by sumjang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+voidft_putchar(char c);
+voidft_swap(int *a, int *b);
+voidft_putstr(char *str);
+intft_strlen(char *str);
+intft_strcmp(char *s1, char *s2);
+
+#endif
