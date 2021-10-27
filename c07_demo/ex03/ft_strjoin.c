9100db182a43bba2fb41a00ac275c531aa3a7fc9
Sumyeong-Jang <50583200+Sumyeong-Jang@users.noreply.github.com>
2021-10-28 06:36:19 +0900

    Update ft_strjoin.c

--- c07_demo/ex03/ft_strjoin.c
+++ c07_demo/ex03/ft_strjoin.c
@@ -12,17 +12,60 @@
 
 #include <stdlib.h>
 
-char	*ft_strjoin(int size, char **strs, char *sep)
+intstr_len(char *str);
+char*str_cpy(char *result, char *str);
+
+char*ft_strjoin(int size, char **strs, char *sep)
 {
-	char	*result;
-	int		idx;
+char*result;
+intlen;
+intidx;
 
-	if (size == 0)
-	{
-		result = (char *)malloc(sizeof(char));
-		return (result);
-	}
-	idx = 0;
-	while (idx < size)
-	{
+if (size == 0)
+return ((char *)malloc(sizeof(char)));
+idx = -1;
+len = 0;
+while (++idx < size)
+len += str_len(strs[idx]);
+len += (str_len(sep) * (size - 1));
+result = (char *)malloc(len * sizeof(char));
+if (!result)
+return (0);
+idx = 0;
+while (idx < size)
+{
+result = str_cpy(result, strs[idx]);
+result = str_cpy(result, sep);
+idx++;
+}
+result = str_cpy(result, strs[idx]);
+return (result);
+}
 
+char*str_cpy(char *result, char *str)
+{
+intidx;
+inti;
+
+idx = 0;
+i = 0;
+while (result[idx])
+idx++;
+while (str[i])
+{
+result[idx + i] = str[i];
+i++;
+}
+result[idx + i] = '\0';
+return (result);
+}
+
+intstr_len(char *str)
+{
+intidx;
+
+idx = 0;
+while (str[idx])
+idx++;
+return (idx);
+}
